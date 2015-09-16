3ae2050bca99e6aa8db0d2fef73777bb8d9b8e70
luismacnunes <luismacnunes@gmail.com>
2015-09-16 11:17:38 +0100

    Background Estática Adicionada

--- index.php
+++ index.php
@@ -4,14 +4,9 @@
 	<title>Zuntuga - Início</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
-<body>
-	<div id="bg">
-		<img src="img/bg.jpg" alt="">
-	</div>
-
-	<h1>Zuntuga</h1>
-	<p/>
-	<br>Bem-vindos ao melhor website de entretenimento!
-
+<body id="background">
+		<h1>Zuntuga</h1>
+		<p/>
+		<br>Bem-vindos ao melhor website de entretenimento!
 </body>
 </html>
